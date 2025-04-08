7340657774ac056b8677239619135ab3bf3f59cd
guialvesds <guilherme_ealves@outlook.com>
2025-04-07 21:40:50 -0300

    Initialing Interface exercice

---
+++ src/interfaces/entities/Installments.java
@@ -0,0 +1,32 @@
+package interfaces.entities;
+
+import java.time.LocalDate;
+
+public class Installments {
+
+    private LocalDate date;
+    private Double amount;
+
+    public Installments(){}
+
+    public Installments(LocalDate date, Double amount){
+        this.date = date;
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+}
